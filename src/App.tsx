4c13833be2b5a4780e8c0f939a65631c1dcdff82
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-21 14:51:16 -0300

    feat: upload cnab file for the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import axios from 'axios';
 import { ReportTransaction } from './model/report-transactions';
 
+const BASE_URL = 'http://localhost:8080';
+
 function App() {
   const [transactions, setTransactions] = useState<ReportTransaction[]>([]);
+  const [file, setFile] = useState<File | null>(null);
 
   const fetchTransactions = async () => {
     const response = await axios.get<ReportTransaction[]>(
-      'http://localhost:8080/transactions'
+      `${BASE_URL}/transactions`
     );
 
     setTransactions(response.data);
+  };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFile(event.target.files ? event.target.files[0] : null);
+  };
+
+  const uploadFile = async () => {
+    const formData = new FormData();
+    if (file) {
+      formData.append('file', file);
+    }
+    axios.post(`${BASE_URL}/cnab/upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
   };
 
   useEffect(() => {
@@ -25,50 +44,48 @@ function App() {
 
       <form>
         <label>Escolha o arquivo</label>
-        <input type="file" accept=".txt" />
+        <input type="file" accept=".txt" onChange={handleFileChange} />
 
-        <button>Enviar arquivo</button>
+        <button onClick={uploadFile}>Enviar arquivo</button>
       </form>
 
       <div>
         <h2>Transações</h2>
 
         <ul>
-          <li>
-            <table>
-              <thead>
-                <tr>
-                  <th>Cartão</th>
-                  <th>CPF</th>
-                  <th>Data</th>
-                  <th>Dono da loja</th>
-                  <th>Hora</th>
-                  <th>Nome da loja</th>
-                  <th>Tipo</th>
-                  <th>Valor</th>
-                </tr>
-              </thead>
+          {transactions.map((report) => (
+            <li key={report.storeName}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Cartão</th>
+                    <th>CPF</th>
+                    <th>Data</th>
+                    <th>Dono da loja</th>
+                    <th>Hora</th>
+                    <th>Nome da loja</th>
+                    <th>Tipo</th>
+                    <th>Valor</th>
+                  </tr>
+                </thead>
 
-              <tbody>
-                {transactions.map((report) => (
-                  <>
-                    {report.transactions.map((transaction) => (
-                      <tr key={transaction.id}>
-                        <td>{transaction.card}</td>
-                        <td>{transaction.cpf}</td>
-                        <td>{transaction.date}</td>
-                        <td>{transaction.storeOwner}</td>
-                        <td>{transaction.time}</td>
-                        <td>{transaction.storeName}</td>
-                        <td>{transaction.type}</td>
-                        <td>{transaction.amount}</td>
-                      </tr>
-                    ))}
-                  </>
-                ))}
-              </tbody>
-            </table>
-          </li>
+                <tbody>
+                  {report.transactions.map((transaction) => (
+                    <tr key={transaction.id}>
+                      <td>{transaction.card}</td>
+                      <td>{transaction.cpf}</td>
+                      <td>{transaction.date}</td>
+                      <td>{transaction.storeOwner}</td>
+                      <td>{transaction.time}</td>
+                      <td>{transaction.storeName}</td>
+                      <td>{transaction.type}</td>
+                      <td>{transaction.amount}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </li>
+          ))}
         </ul>
       </div>
     </main>
